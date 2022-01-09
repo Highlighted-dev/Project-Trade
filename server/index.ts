035f31c5a1c0d230035eff4111f00a073001865c
Highlighted-dev <bartosz.korz@gmail.com>
2022-01-09 14:03:06 +0100

    Database setup

--- server/index.ts
+++ server/index.ts
@@ -2,4 +2,14 @@ import express from 'express';
 import mongoose from 'mongoose';
 
 const app = express();
+const url = 'mongodb://127.0.0.1:27017';
 
+mongoose.connect(url)
+
+const db = mongoose.connection
+db.once('open', _ => {
+    console.log('Database connected: ',url)
+})
+db.on('error',err => {
+    console.log('Connection falied: ',err)
+})
