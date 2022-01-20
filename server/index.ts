e451f610fb4b245fcee44eefeeedcf8a8baa03ca
Highlighted-dev <bartosz.korz@gmail.com>
2022-01-20 22:46:06 +0100

    Added new message when successfuly launched an app

--- server/index.ts
+++ server/index.ts
@@ -10,6 +10,6 @@ app.get('/', (req: Request, res: Response ) =>{
 const url: string = 'mongodb://127.0.0.1:27017';
 const port: number = 5000
 
-mongoose.connect(url).then(() => app.listen(port, () => console.log('Database connected: ',url))).catch((error) => console.log(error.message))
+mongoose.connect(url).then(() => app.listen(port, () => console.log('Database connected: ',url,' | Its alive on http://localhost:'+port))).catch((error) => console.log(error.message))
 
 const db: Connection = mongoose.connection
