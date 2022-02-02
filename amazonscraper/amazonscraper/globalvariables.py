eddc02221a3005e025d0dbd9e7a171c1bbc2e242
Highlighted-dev <bartosz.korz@gmail.com>
2022-02-02 22:03:12 +0100

    Moved all important variables t globalvariables.py

---
+++ amazonscraper/amazonscraper/globalvariables.py
@@ -0,0 +1,15 @@
+#Here are all important variables that you would like to change.
+start_urls = [
+        'https://www.amazon.de/s?rh=n%3A571860&s=review-count-rank',
+        #'https://www.amazon.de/s?rh=n%3A571954&s=review-count-rank',
+        #'https://www.amazon.de/s?rh=n%3A1966060031&s=review-count-rank',
+        #'https://www.amazon.de/s?rh=n%3A340849031&s=review-count-rank',
+        #'https://www.amazon.de/s?rh=n%3A236861011&s=review-count-rank',
+    ]
+allowed_domains = ['amazon.de','amazon.fr']
+pathToJson =('C:/Programowanie/Projekty/Project-Trade/amazonscraper/file.json')
+
+#MongoDb related stuff
+mongoUrl = 'localhost:27017'
+mongoDatabase = 'ProjectTrade'
+mongoColumn = 'amazonProductData'
