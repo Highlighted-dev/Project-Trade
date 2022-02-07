f6b21ab6f710d86ed796482fa6f588605acce3de
Highlighted-dev <bartosz.korz@gmail.com>
2022-02-08 00:08:10 +0100

    Clicked "enter" two times to make file look better

--- server/routes/amazonProductDataRoute.ts
+++ server/routes/amazonProductDataRoute.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response, Router } from "express";
 import amazonProductData from "../models/amazonProductDataModel";
+
 const router: Router = express.Router(); 
+
 router.get('/', async (req: Request,res: Response) => {
     const data = await amazonProductData.find();
     res.json(data);
