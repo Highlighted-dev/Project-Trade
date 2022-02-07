ced4fc09693b16d26bd7c36df7232cc7b05bcf9d
Highlighted-dev <bartosz.korz@gmail.com>
2022-02-08 00:08:56 +0100

    Scraper can now scrape up to 99 pages from 1 url.

--- amazonscraper/amazonscraper/spiders/AmazonProductSpider.py
+++ amazonscraper/amazonscraper/spiders/AmazonProductSpider.py
@@ -1,4 +1,5 @@
 from array import array
+from asyncio.windows_events import NULL
 import scrapy
 from amazonscraper.items import AmazonscraperItem
 import os
@@ -11,7 +12,7 @@ class AmazonproductspiderSpider(scrapy.Spider):
     name = 'AmazonProductSpider'
     allowed_domains = globalvariables.allowed_domains 
     start_urls = globalvariables.start_urls
-
+   
     def parse(self, response):
         items = AmazonscraperItem()
         #Getting data from amazon
@@ -24,19 +25,33 @@ class AmazonproductspiderSpider(scrapy.Spider):
             #Storing all data into items
             items['_id'] = ''.join(prod_id[i]).strip()
             items['product_name'] = ''.join(title[i]).strip()
-            items['product_sale_price'] = ''.join(sale_price[i]).strip()
+            try:
+                items['product_sale_price'] = ''.join(sale_price[i]).strip()
+            except:
+                items['product_sale_price'] = NULL
             items['product_image'] = ''.join(prod_image[i]).strip()
             yield items
-    
-    # def closed(self, reason):
-    #     #Send file.json to database when finished scraping
-    #     path = globalvariables.pathToJson
-    #     assert os.path.isfile(path)
-    #     myclient = pymongo.MongoClient(globalvariables.mongoUrl)
-    #     mydb = myclient[globalvariables.mongoDatabase]
-    #     mycol = mydb[globalvariables.mongoColumn]
-    #     with open(path) as f:
-    #         file_data = json.load(f)
-    #     mycol.insert_many(file_data)
+        #Get current amazon (ex. amazon.de)
+        current_amazon = str(response.request.url).split("/-",1)[0]
+        print(current_amazon)
+        #Get next page URL
+        next_page=current_amazon+"".join(response.xpath('//div[@class="a-section a-text-center s-pagination-container"]//a[@class="s-pagination-item s-pagination-next s-pagination-button s-pagination-separator"]/@href').extract())
+        if str(next_page[-2:len(next_page)]) != globalvariables.maxPagesPerCategoryString:
+            yield scrapy.Request(next_page, callback=self.parse)
+
+    def closed(self, reason):
+        #Send file.json to database when finished scraping
+        path = globalvariables.pathToJson
+        assert os.path.isfile(path)
+        myclient = pymongo.MongoClient(globalvariables.mongoUrl)
+        mydb = myclient[globalvariables.mongoDatabase]
+        mycol = mydb[globalvariables.mongoColumn]
+        with open(path) as f:
+            file_data = json.load(f)
+        #TODO change mongodb so it will skip same items. Right now if any item from json is in database "except" will show
+        try:
+            mycol.insert_many(file_data)
+        except:
+            print("Some items from file.json are already in database.")
 
         
