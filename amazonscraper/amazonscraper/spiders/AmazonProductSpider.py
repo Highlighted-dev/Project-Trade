0db79d80d6c1ee95e5be3ab687e0951e3004afca
Highlighted-dev <bartosz.korz@gmail.com>
2022-02-16 23:25:38 +0100

    Scraper now supports running in multiple consoles

--- amazonscraper/amazonscraper/spiders/AmazonProductSpider.py
+++ amazonscraper/amazonscraper/spiders/AmazonProductSpider.py
@@ -7,40 +7,45 @@ import pymongo
 import json
 from .. import globalvariables
 class AmazonproductspiderSpider(scrapy.Spider):
-    if os.path.exists("file.json"):
-        os.remove("file.json")
+    def __init__(self, consoleNumber, maxConsoleNumber):
+        self.start_urls = globalvariables.start_urls[int(consoleNumber)*int(len(globalvariables.start_urls)/int(maxConsoleNumber)):(int(consoleNumber)+1)*int(len(globalvariables.start_urls)/int(maxConsoleNumber))]
+        global pathToJson
+        pathToJson = f'C:/Programowanie/Projekty/Project-Trade/amazonscraper/file{consoleNumber}.json'
+        if os.path.exists(f"file{consoleNumber}.json"):
+                os.remove(f"file{consoleNumber}.json")
     name = 'AmazonProductSpider'
-    allowed_domains = globalvariables.allowed_domains 
-    start_urls = globalvariables.start_urls
-   
+    allowed_domains = globalvariables.allowed_domains
     def parse(self, response):
-        items = AmazonscraperItem()
-        #Getting data from amazon
-        prod_id = response.xpath('//div[@class="sg-col-4-of-12 s-result-item s-asin sg-col-4-of-16 sg-col s-widget-spacing-small sg-col-4-of-20"]/@data-asin').extract()
-        title = response.xpath('//div[@class="a-section a-spacing-none a-spacing-top-small s-title-instructions-style"]//span[@class="a-size-base-plus a-color-base a-text-normal"]/text()').extract()
-        sale_price = response.xpath('//div[@class="a-section a-spacing-none a-spacing-top-small s-price-instructions-style"]//span[@class="a-price-whole"]/text()').extract()
-        prod_image = response.xpath('//div[@class="a-section aok-relative s-image-square-aspect"]//img[@class="s-image"]/@src').extract()
-        #27-3 = 24, [0,1,2..23]
-        for i in range(len(title)- 3):
-            #Storing all data into items
-            items['_id'] = ''.join(prod_id[i]).strip()
-            items['product_name'] = ''.join(title[i]).strip()
-            try:
-                items['product_sale_price'] = ''.join(sale_price[i]).strip()
-            except:
-                items['product_sale_price'] = NULL
-            items['product_image'] = ''.join(prod_image[i]).strip()
-            yield items
+        try:
+            items = AmazonscraperItem()
+            #Getting data from amazon
+            prod_id = response.xpath('//div[@class="sg-col-4-of-12 s-result-item s-asin sg-col-4-of-16 sg-col s-widget-spacing-small sg-col-4-of-20"]/@data-asin').extract()
+            title = response.xpath('//div[@class="a-section a-spacing-none a-spacing-top-small s-title-instructions-style"]//span[@class="a-size-base-plus a-color-base a-text-normal"]/text()').extract()
+            sale_price = response.xpath('//div[@class="a-section a-spacing-none a-spacing-top-small s-price-instructions-style"]//span[@class="a-price-whole"]/text()').extract()
+            prod_image = response.xpath('//div[@class="a-section aok-relative s-image-square-aspect"]//img[@class="s-image"]/@src').extract()
+
+            for i in range(globalvariables.itemsPerPage):
+                #Storing all data into items
+                items['_id'] = ''.join(prod_id[i]).strip()
+                items['product_name'] = ''.join(title[i]).strip()
+                try:
+                    items['product_sale_price'] = ''.join(sale_price[i]).strip()
+                except:
+                    items['product_sale_price'] = NULL
+                items['product_image'] = ''.join(prod_image[i]).strip()
+                yield items
+        except Exception as e:
+            print("Something went wrong when extracting items\n",e)
         #Get current amazon (ex. amazon.de)
         current_amazon = str(response.request.url).split("/-",1)[0]
         #Get next page URL
         next_page=current_amazon+"".join(response.xpath('//div[@class="a-section a-text-center s-pagination-container"]//a[@class="s-pagination-item s-pagination-next s-pagination-button s-pagination-separator"]/@href').extract())
-        if str(next_page[-2:len(next_page)]) != globalvariables.maxPagesPerCategoryString:
+        if str(next_page[-2:len(next_page)]) != globalvariables.maxPagesPerCategoryString and int(globalvariables.maxPagesPerCategoryString) > 1:
             yield scrapy.Request(next_page, callback=self.parse)
 
     def closed(self, reason):
         #Send file.json to database when finished scraping
-        path = globalvariables.pathToJson
+        path = pathToJson
         assert os.path.isfile(path)
         myclient = pymongo.MongoClient(globalvariables.mongoUrl)
         mydb = myclient[globalvariables.mongoDatabase]
