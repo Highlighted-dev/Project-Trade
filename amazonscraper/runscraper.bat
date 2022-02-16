dbb109b1590767061dbb33f663181c6a310e8f53
Highlighted-dev <bartosz.korz@gmail.com>
2022-02-16 23:25:08 +0100

    Scraper can be launched now from this file.

---
+++ amazonscraper/runscraper.bat
@@ -0,0 +1,4 @@
+::consoleNuber: what console is now launching, simlar to id | maxConsoleNumber: total number of consoles that will be/are launching 
+start cmd.exe /c "scrapy crawl AmazonProductSpider -o file0.json -a consoleNumber=0 -a maxConsoleNumber=3"
+start cmd.exe /c "scrapy crawl AmazonProductSpider -o file1.json -a consoleNumber=1 -a maxConsoleNumber=3"
+start cmd.exe /c "scrapy crawl AmazonProductSpider -o file2.json -a consoleNumber=2 -a maxConsoleNumber=3"
