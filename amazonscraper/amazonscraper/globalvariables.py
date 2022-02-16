85028e059439d0195854e87b23d842c74d213894
Highlighted-dev <bartosz.korz@gmail.com>
2022-02-16 23:26:30 +0100

    Added itemsPerPage int, deleted pathToJson - now is declared as global variable in AmazonProductSpider.py

--- amazonscraper/amazonscraper/globalvariables.py
+++ amazonscraper/amazonscraper/globalvariables.py
@@ -1,16 +1,21 @@
 #Here are all important variables that you would like to change.
 start_urls = [
         'https://www.amazon.de/s?rh=n%3A571860&s=review-count-rank',
-        #'https://www.amazon.de/s?rh=n%3A571954&s=review-count-rank',
-        #'https://www.amazon.de/s?rh=n%3A1966060031&s=review-count-rank',
-        #'https://www.amazon.de/s?rh=n%3A340849031&s=review-count-rank',
-        #'https://www.amazon.de/s?rh=n%3A236861011&s=review-count-rank',
+        'https://www.amazon.de/s?rh=n%3A571954&s=review-count-rank',
+        'https://www.amazon.de/s?rh=n%3A1966060031&s=review-count-rank',
+        'https://www.amazon.de/s?rh=n%3A340849031&s=review-count-rank',
+        'https://www.amazon.de/s?rh=n%3A571860&s=review-count-rank',
+        'https://www.amazon.de/s?rh=n%3A571954&s=review-count-rank',
+        'https://www.amazon.de/s?rh=n%3A1966060031&s=review-count-rank',
+        'https://www.amazon.de/s?rh=n%3A340849031&s=review-count-rank',
+        'https://www.amazon.de/s?rh=n%3A571860&s=review-count-rank',
     ]
 allowed_domains = ['amazon.de','amazon.fr']
-maxPagesPerCategoryString = "10" #Up to 99
-pathToJson =('C:/Programowanie/Projekty/Project-Trade/amazonscraper/file.json')
+maxPagesPerCategoryString = "1" #Up to 99
+itemsPerPage = 24
 
-#MongoDb related stuff
+
+#MongoDb related variables
 mongoUrl = 'localhost:27017'
 mongoDatabase = 'ProjectTrade'
 mongoColumn = 'amazonProductData'
