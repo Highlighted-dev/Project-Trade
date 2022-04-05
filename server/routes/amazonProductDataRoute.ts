96dbe76182962681021006126791e560ee6bd796
Highlighted-dev <bartosz.korz@gmail.com>
2022-04-05 13:51:02 +0200

    Added new endpoint: Find product by name

--- server/routes/amazonProductDataRoute.ts
+++ server/routes/amazonProductDataRoute.ts
@@ -1,14 +1,23 @@
-import express, { Request, Response, Router } from "express";
-import amazonProductData from "../models/amazonProductDataModel";
+import express, { Request, Response, Router } from 'express';
+import amazonProductData from '../models/amazonProductDataModel';
 
-const router: Router = express.Router(); 
+const router: Router = express.Router();
 
-router.get('/as', async (req: Request,res: Response) => {
-    res.json(await amazonProductData.find());
-})
-router.post('/as/:id', async (req: Request, res: Response) => {
-    const { id } = req.params
-    res.json(await amazonProductData.findById(id))
-})
-
+router.get('/as', async (req: Request, res: Response) => {
+  res.json(await amazonProductData.find());
+});
+//Get product by id
+router.post('/as/id/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  res.json(await amazonProductData.findById(id));
+});
+//Get product by name
+router.post('/as/name/:name', async (req: Request, res: Response) => {
+  const { name } = req.params;
+  res.json(
+    await amazonProductData.find({
+      product_name: { $regex: '^' + name, $options: 'i' },
+    })
+  );
+});
 export default router;
