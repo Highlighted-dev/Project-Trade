65fa648dde3bc1a8e5d488b7d45772ad6586370b
Highlighted-dev <bartosz.korz@gmail.com>
2022-05-06 14:48:45 +0200

    Added captcha checker

--- amazonscraper/amazonscraper/spiders/AmazonOneProductSpider.py
+++ amazonscraper/amazonscraper/spiders/AmazonOneProductSpider.py
@@ -1,5 +1,4 @@
 import scrapy
-from sqlalchemy import null
 from .. import GlobalVariables
 import os
 from amazonscraper.items import AmazonScraperOneItem
@@ -10,11 +9,24 @@ class AmazonOneProductSpider(scrapy.Spider):
     allowed_domains = GlobalVariables.allowed_domains
     def parse(self, response):
         try:
-            items = AmazonScraperOneItem()
-            #Getting data from amazon
-            productName = response.xpath('//div[@id="titleSection"]//span[@id="productTitle"]/text()').extract()
-            items['product_name'] = "".join(productName).strip()
-            print(productName)
-            yield items
+            if self.checkForCaptcha(response):
+                print("Solving captcha...")
+            else:
+                items = AmazonScraperOneItem()
+                #Getting data from amazon
+                product_name = response.xpath('//div[@id="titleSection"]//span[@id="productTitle"]/text()').extract()
+                product_image = response.xpath('//div[id="altImages"]//li[@class="a-spacing-small item imageThumbnail a-declarative"]//span[@class="a-button-text"]/img/@src').extract()
+                print(product_image)
+                items['product_name'] = "".join(product_name).strip()
+                yield items
         except Exception as e:
             print("Something went wrong when extracting items\n",e)
+
+    def checkForCaptcha(self, response):
+        captcha_url = response.xpath('//div[@class="a-row a-text-center"]/img/@src').extract_first()
+
+        if captcha_url:
+            self.logger.info('PAGE {} GOT BY CAPTCHA!'.format(response.request.url))
+            return True
+        else:
+            return False
