f4901c6ef5d9409aab589cea77ebc5a14ffca879
Highlighted-dev <bartosz.korz@gmail.com>
2022-05-07 19:12:32 +0200

    Added new items for AmazonOneProductSpider

--- amazonscraper/amazonscraper/items.py
+++ amazonscraper/amazonscraper/items.py
@@ -12,10 +12,21 @@ class AmazonscraperItem(scrapy.Item):
   product_name = scrapy.Field()
   product_sale_price = scrapy.Field()
   product_image = scrapy.Field()
-class AmazonScraperOneItem(scrapy.Item):
-  #_id=proudct_id | Name changed because mongodb was assigning default values for _id
-  _id = scrapy.Field()
-  product_name = scrapy.Field()
-  product_sale_price = scrapy.Field()
+  mongo_db_column_name = scrapy.Field()
+class AmazonItemImages(scrapy.Item):
+  mongo_db_column_name = scrapy.Field()
+  product_id = scrapy.Field()
   product_image = scrapy.Field()
+class AmazonItemDetails(scrapy.Item):
+  mongo_db_column_name = scrapy.Field()
+  product_id = scrapy.Field()
+  product_detail_name = scrapy.Field()
+  product_detail = scrapy.Field()
+class AmazonItemTechnicalDetails(scrapy.Item):
+  mongo_db_column_name = scrapy.Field()
+  product_id = scrapy.Field()
+  product_technical_detail_name = scrapy.Field()
+  product_technical_detail = scrapy.Field()
+
+
 
