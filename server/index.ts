85152dff13ac8ef67889b3ea368cae1d94ede14a
Highlighted-dev <bartosz.korz@gmail.com>
2022-05-09 18:32:08 +0200

    Added new endpoint for app

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,25 @@
 import express, { Application, Response, Request } from 'express';
 import mongoose, { Connection } from 'mongoose';
-import amazonProductDataRoute from './routes/amazonProductDataRoute'
-const app: Application = express(); 
+import AmazonProductRoute from './routes/AmazonProductRoute';
+import AmazonScraperRoute from './routes/AmazonScraperRoute';
+const app: Application = express();
 //as=amazonScraper
 
-
-
 const url: string = 'mongodb://localhost:27017/ProjectTrade';
-const port: number = 5000
+const port: number = 5000;
 
-mongoose.connect(url).then(() => app.listen(port, () => console.log('Database connected: ',url,' | Its alive on http://localhost:'+port))).catch((error) => console.log(error.message))
+mongoose
+  .connect(url)
+  .then(() =>
+    app.listen(port, () =>
+      console.log(
+        'Database connected: ',
+        url,
+        ' | Its alive on http://localhost:' + port
+      )
+    )
+  )
+  .catch(error => console.log(error.message));
 
-app.use('/api',amazonProductDataRoute)
+app.use('/api/ap', AmazonProductRoute);
+app.use('/api/as', AmazonScraperRoute);
