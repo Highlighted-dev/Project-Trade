43d5038aadc3839e9d236fb1f0b700472c9919c4
Highlighted-dev <bartosz.korz@gmail.com>
2022-05-12 22:32:15 +0200

    Now if there's no product details in database, express redirects to amazon scraper (as) endpoint for data

--- server/routes/AmazonProductRoute.ts
+++ server/routes/AmazonProductRoute.ts
@@ -3,7 +3,16 @@ import AmazonProductData from '../models/AmazonProductDataModel';
 import AmazonProductDetails from '../models/AmazonProductDetailsModel';
 import AmazonProductImages from '../models/AmazonProductImagesModel';
 import AmazonProductTechnicalDetails from '../models/AmazonProductTechnicalDetailsModel';
+import session from 'express-session';
+import { Model } from 'mongoose';
+import amazonProductImages from '../models/AmazonProductImagesModel';
 
+//Session variables interface for typescript
+declare module 'express-session' {
+  export interface SessionData {
+    url: string;
+  }
+}
 const router: Router = express.Router();
 
 router.get('/', async (req: Request, res: Response) => {
@@ -23,28 +32,35 @@ router.get('/name/:name', async (req: Request, res: Response) => {
     })
   );
 });
-router.get('/details/id/:id', async (req: Request, res: Response) => {
+//Get details by id
+router.get('/details/id/:id', async (req: Request, res: Response) =>
+  getAmazonDetailedData(req, res, AmazonProductDetails)
+);
+//Get images by id
+router.get('/images/id/:id', (req: Request, res: Response) =>
+  getAmazonDetailedData(req, res, amazonProductImages)
+);
+//Get technical details by id
+router.get('/technicalDetails/id/:id', (req: Request, res: Response) =>
+  getAmazonDetailedData(req, res, AmazonProductTechnicalDetails)
+);
+
+const getAmazonDetailedData = async (
+  req: Request,
+  res: Response,
+  modelName: Model<any, any, any, any>
+) => {
   const { id } = req.params;
-  res.json(
-    await AmazonProductDetails.find({
-      product_id: id,
-    })
-  );
-});
-router.get('/images/id/:id', async (req: Request, res: Response) => {
-  const { id } = req.params;
-  res.json(
-    await AmazonProductImages.find({
-      product_id: id,
-    })
-  );
-});
-router.get('/technicalDetails/id/:id', async (req: Request, res: Response) => {
-  const { id } = req.params;
-  res.json(
-    await AmazonProductTechnicalDetails.find({
-      product_id: id,
-    })
-  );
-});
+  const amazon_product_detailed_data = await modelName.find({
+    product_id: id,
+  });
+  //If json is empty
+  if (amazon_product_detailed_data.length > 3) {
+    res.json(amazon_product_detailed_data);
+  } else {
+    //Store current url in express sessions
+    req.session.url = req.originalUrl;
+    res.redirect('/api/as/id/' + id);
+  }
+};
 export default router;
