cdd6199bb5aaf1e519c11651ca5a025622792c92
Highlighted-dev <bartosz.korz@gmail.com>
2022-05-13 21:09:32 +0200

    Every request is now splashrequest, now if theres no technical details scraper will take product about

--- amazonscraper/amazonscraper/spiders/AmazonOneProductSpider.py
+++ amazonscraper/amazonscraper/spiders/AmazonOneProductSpider.py
@@ -1,10 +1,10 @@
 import scrapy
 from .. import GlobalVariables
 import os
-from amazonscraper.items import AmazonItemImages, AmazonItemDetails, AmazonItemTechnicalDetails
+from amazonscraper.items import AmazonItemImages, AmazonItemDetails, AmazonItemTechnicalDetails, AmazonItemAbout
 from amazoncaptcha import AmazonCaptcha
 import logging
-from scrapy_splash import SplashFormRequest
+from scrapy_splash import SplashFormRequest, SplashRequest
 import pymongo
 import json
 class AmazonOneProductSpider(scrapy.Spider):
@@ -12,6 +12,10 @@ class AmazonOneProductSpider(scrapy.Spider):
         global product_id
         product_id = prod_id
         self.start_urls = ["https://www.amazon.de/-/en/dp/"+product_id]
+    #Change every request from ScrapyRequest to SplashRequest for javascript load
+    def start_requests(self):
+        for url in self.start_urls:
+            yield SplashRequest(url, self.parse)
     name = 'AmazonOneProductSpider'
     allowed_domains = GlobalVariables.allowed_domains
     def parse(self, response):
@@ -22,14 +26,16 @@ class AmazonOneProductSpider(scrapy.Spider):
                 images = AmazonItemImages()
                 details = AmazonItemDetails()
                 technical_details = AmazonItemTechnicalDetails()
+                abouts = AmazonItemAbout()
                 #Getting data from amazon
+
                 #product_name = response.xpath('//div[@id="titleSection"]//span[@id="productTitle"]/text()').extract()
                 product_images = response.xpath('//div[@id="altImages"]//li[@class="a-spacing-small item imageThumbnail a-declarative"]//span[@class="a-button-text"]//img/@src').extract()
                 product_details_name = response.xpath('//div[@id="productOverview_feature_div"]//table[@class="a-normal a-spacing-micro"]//td[@class="a-span3"]//span/text()').extract()                
                 product_details = response.xpath('//div[@id="productOverview_feature_div"]//table[@class="a-normal a-spacing-micro"]//td[@class="a-span9"]//span/text()').extract()  
                 product_technical_details_name = response.xpath('//div[@id="prodDetails"]//table[@id="productDetails_techSpec_section_1"]//th/text()').extract()
                 product_technical_details = response.xpath('//div[@id="prodDetails"]//table[@id="productDetails_techSpec_section_1"]//td/text()').extract()
-
+                
                 for image in product_images:
                     images['product_id'] = product_id
                     images['product_image'] = image
@@ -42,16 +48,26 @@ class AmazonOneProductSpider(scrapy.Spider):
                     details['product_detail'] = pd
                     details['mongo_db_column_name'] = GlobalVariables.mongo_column_details
                     yield details
-                for ptd,ptdn in zip(product_technical_details,product_technical_details_name):   
-                    #ptd = product_techinal_detail | ptdn = product_technial_detail_name
-                    technical_details['product_id'] = product_id
-                    technical_details['product_technical_detail_name'] = "".join(ptdn).strip()
-                    ptd_encode = ptd.encode("ascii","ignore")
-                    ptd = ptd_encode.decode()
-                    technical_details['product_technical_detail'] = "".join(ptd).strip()
-                    technical_details['mongo_db_column_name'] = GlobalVariables.mongo_column_technical_details
-                    yield technical_details
-                # yield items
+
+                #Sometimes there aren't any technical details
+                if len(product_technical_details) < 1:
+                    product_abouts = response.xpath('//div[@id="feature-bullets"]//ul[@class="a-unordered-list a-vertical a-spacing-mini"]//li[not(@id) and not(@class)]//span[@class="a-list-item"]/text()').extract()
+                    for about in product_abouts:
+                        abouts['product_id'] = product_id
+                        abouts['product_about'] = about
+                        abouts['mongo_db_column_name'] = GlobalVariables.mongo_column_about
+                        yield abouts
+                else:
+                    for ptd,ptdn in zip(product_technical_details,product_technical_details_name):   
+                        #ptd = product_techinal_detail | ptdn = product_technial_detail_name
+                        technical_details['product_id'] = product_id
+                        technical_details['product_technical_detail_name'] = "".join(ptdn).strip()
+                        #Some technical details have ascii characters, we want to get rid of that
+                        ptd_encode = ptd.encode("ascii","ignore")
+                        ptd = ptd_encode.decode()
+                        technical_details['product_technical_detail'] = "".join(ptd).strip()
+                        technical_details['mongo_db_column_name'] = GlobalVariables.mongo_column_technical_details
+                        yield technical_details
         except Exception as e:
             logging.error("Something went wrong while extracting items\n")
             logging.error(e)
@@ -65,7 +81,7 @@ class AmazonOneProductSpider(scrapy.Spider):
         else:
             return False
     def solveCaptcha(self, response, origin_method):
-        logging.log(logging.INFO, "Trying to solve captcha...")
+        logging.info("Trying to solve captcha...")
         try:
             captcha_url = response.xpath('//div[@class="a-row a-text-center"]/img/@src').extract_first()
             captcha = AmazonCaptcha.fromlink(captcha_url)
