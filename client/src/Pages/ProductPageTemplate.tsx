88cc8b472699f51fd81dbc6e15f40c6e0d53b91e
Highlighted-dev <bartosz.korz@gmail.com>
2022-05-20 21:37:20 +0200

    Made function from repeating fetch requests

--- client/src/Pages/ProductPageTemplate.tsx
+++ client/src/Pages/ProductPageTemplate.tsx
@@ -7,29 +7,65 @@ const ProductWebsiteTemplate = () => {
   const [details, setDetails] = useState<any[]>([]);
   const [images, setImages] = useState<any[]>([]);
   const [technicalDetails, setTechnicalDetails] = useState<any[]>([]);
+  const [about, setAbout] = useState<any[]>([]);
+
+  const clearStates = () => {
+    setImages([]);
+    setDetails([]);
+    setTechnicalDetails([]);
+    setAbout([]);
+  };
+  const fetchProductData = (
+    requestOptions: RequestInit,
+    url: string,
+    setProductData: (arg0: never[]) => void,
+  ) => {
+    fetch(url + productId, requestOptions)
+      .then(async response => await response.json())
+      .then(data => {
+        //If json is not empty
+        if (data.length > 2) setProductData(data);
+        else setProductData([]);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+    fetch('/api/ap/about/id/' + productId, requestOptions)
+      .then(async response => await response.json())
+      .then(data => {
+        setAbout(data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
   useEffect(() => {
     setChangingProductId(productId);
-    const searchbar = document.querySelector('.searchBar');
+    clearStates();
     const requestOptions = {
       method: 'GET',
     };
+
+    fetch('/api/ap/checkProduct/id/' + productId, requestOptions)
+      .then(async response => {
+        if (response.status === 200) {
+          return true;
+        } else {
+          return Promise.reject();
+        }
+      })
+      .then(data => {
+        console.log(data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+
     //Get product data with "x" name from api
-    //TODO Change this to one big request?
-    fetch('/api/ap/details/id/' + productId, requestOptions)
-      .then(async response => setDetails(await response.json()))
-      .catch(e => {
-        console.log(e);
-      });
-    fetch('/api/ap/images/id/' + productId, requestOptions)
-      .then(async response => setImages(await response.json()))
-      .catch(e => {
-        console.log(e);
-      });
-    fetch('/api/ap/technicalDetails/id/' + productId, requestOptions)
-      .then(async response => setTechnicalDetails(await response.json()))
-      .catch(e => {
-        console.log(e);
-      });
+    fetchProductData(requestOptions, '/api/ap/images/id/', setImages);
+    fetchProductData(requestOptions, '/api/ap/technicalDetails/id/', setTechnicalDetails);
+    fetchProductData(requestOptions, '/api/ap/details/id/', setDetails);
+    fetchProductData(requestOptions, '/api/ap/about/id/', setAbout);
   }, [productId]);
 
   return (
@@ -37,29 +73,37 @@ const ProductWebsiteTemplate = () => {
       <h2>{productId}</h2>
       <br />
       <ul>
-        {details.map(product => (
-          <li key={product._id}>
-            {product.product_detail_name}: {product.product_detail}
-          </li>
-        ))}
+        {details.length < 3 ? (
+          <li>loading data...</li>
+        ) : (
+          details.map(product => (
+            <li key={product._id}>
+              {product.product_detail_name}: {product.product_detail}
+            </li>
+          ))
+        )}
       </ul>
       <ul>
-        {images.length > 0 ? (
+        {images.length < 3 ? (
+          <li>loading data...</li>
+        ) : (
           images.map(product => (
             <li key={product._id}>
               image: <img src={product.product_image} />
             </li>
           ))
-        ) : (
-          <li>XD</li>
         )}
       </ul>
       <ul>
-        {technicalDetails.map(product => (
-          <li key={product._id}>
-            {product.product_technical_detail_name}: {product.product_technical_detail}
-          </li>
-        ))}
+        {details.length < 3 ? (
+          <li>loading data...</li>
+        ) : (
+          technicalDetails.map(product => (
+            <li key={product._id}>
+              {product.product_technical_detail_name}: {product.product_technical_detail}
+            </li>
+          ))
+        )}
       </ul>
     </div>
   );
