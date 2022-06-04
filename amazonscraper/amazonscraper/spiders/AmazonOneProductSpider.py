9654d790a0db5117aaa864276057476d82c3eb94
Highlighted-dev <bartosz.korz@gmail.com>
2022-06-04 23:39:55 +0200

    Thumbnail images are now back to AmazonOneProductSpider

--- amazonscraper/amazonscraper/spiders/AmazonOneProductSpider.py
+++ amazonscraper/amazonscraper/spiders/AmazonOneProductSpider.py
@@ -1,11 +1,9 @@
 import scrapy
 from .. import GlobalVariables
-import os
-from amazonscraper.items import AmazonItemImages, AmazonItemDetails, AmazonItemTechnicalDetails, AmazonItemAbout
+from amazonscraper.items import AmazonItemThumbImages, AmazonItemDetails, AmazonItemTechnicalDetails, AmazonItemAbout
 from amazoncaptcha import AmazonCaptcha
 import logging
 from scrapy_splash import SplashFormRequest, SplashRequest
-import re
 class AmazonOneProductSpider(scrapy.Spider):
     def __init__(self, prod_id):
         global product_id
@@ -22,18 +20,24 @@ class AmazonOneProductSpider(scrapy.Spider):
             if self.checkForCaptcha(response):
                 yield from self.solveCaptcha(response, self.parse)
             else:
-                images = AmazonItemImages()
+                images = AmazonItemThumbImages()
                 details = AmazonItemDetails()
                 technical_details = AmazonItemTechnicalDetails()
                 abouts = AmazonItemAbout()
 
                 #Getting data from amazon
                 #product_name = response.xpath('//div[@id="titleSection"]//span[@id="productTitle"]/text()').extract()
+                product_images = response.xpath('//div[@id="altImages"]//li[@class="a-spacing-small item imageThumbnail a-declarative"]//span[@class="a-button-text"]//img/@src').extract()
                 product_details_name = response.xpath('//div[@id="productOverview_feature_div"]//table[@class="a-normal a-spacing-micro"]//td[@class="a-span3"]//span/text()').extract()                
                 product_details = response.xpath('//div[@id="productOverview_feature_div"]//table[@class="a-normal a-spacing-micro"]//td[@class="a-span9"]//span/text()').extract()  
                 product_technical_details_name = response.xpath('//div[@id="prodDetails"]//table[@id="productDetails_techSpec_section_1"]//th/text()').extract()
                 product_technical_details = response.xpath('//div[@id="prodDetails"]//table[@id="productDetails_techSpec_section_1"]//td/text()').extract()
 
+                for image in product_images:
+                    images['product_id'] = product_id
+                    images['product_thumb_image'] = image
+                    images['mongo_db_column_name'] = GlobalVariables.mongo_column_thumb_images
+                    yield images
                 #Sometimes there aren't any product details
                 if len(product_details) < 1:
                     product_abouts = response.xpath('//div[@id="feature-bullets"]//ul[@class="a-unordered-list a-vertical a-spacing-mini"]//li[not(@id) and not(@class)]//span[@class="a-list-item"]/text()').extract()
@@ -76,7 +80,6 @@ class AmazonOneProductSpider(scrapy.Spider):
 
     def checkForCaptcha(self, response):
         captcha_url = response.xpath('//div[@class="a-row a-text-center"]/img/@src').extract_first()
-
         if captcha_url:
             logging.info('Found captcha on page!')
             return True
