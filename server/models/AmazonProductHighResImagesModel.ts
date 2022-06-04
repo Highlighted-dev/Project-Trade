4702366670dbed6e2c287126e691a34f466693b9
Highlighted-dev <bartosz.korz@gmail.com>
2022-06-05 00:04:21 +0200

    Created AmazonProductHighResImagesModel.ts

---
+++ server/models/AmazonProductHighResImagesModel.ts
@@ -0,0 +1,21 @@
+import { ObjectId } from 'mongodb';
+import mongoose, { Model, Schema } from 'mongoose';
+
+interface IProductHighResImages {
+  _id: ObjectId;
+  product_id: string;
+  product_highres_image: string;
+}
+
+const amazonProductHighResImagesSchema = new Schema<IProductHighResImages>(
+  {
+    product_id: String,
+    product_highres_image: String,
+  },
+  { collection: 'amazonProductHighResImages' }
+);
+var amazonProductHighResImages: Model<IProductHighResImages> = mongoose.model(
+  'amazonProductHighResImages',
+  amazonProductHighResImagesSchema
+);
+export default amazonProductHighResImages;
