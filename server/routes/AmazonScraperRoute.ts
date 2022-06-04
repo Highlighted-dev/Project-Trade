36109133f6bc26587a33c18b9c14e1a1b91dd816
Highlighted-dev <bartosz.korz@gmail.com>
2022-06-05 00:27:55 +0200

    Added endpoint for high resolution images

--- server/routes/AmazonScraperRoute.ts
+++ server/routes/AmazonScraperRoute.ts
@@ -25,5 +25,27 @@ router.get('/id/:id', async (req: Request, res: Response) => {
     }
   );
 });
+router.get('/highres/id/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  /**
+   * TODO: Run this only when theres no items with that id in database
+   * */
+  const { exec } = require('child_process'); //TODO: Change this to import
+  exec(
+    'cd ../amazonscraper & scrapy crawl AmazonGetHighResImages -a prod_id="' +
+      id +
+      '"',
+    function (error: Error, stderr: stream.Readable, stdout: stream.Readable) {
+      if (error) {
+        console.log(error);
+        res.sendStatus(500);
+      } else {
+        console.log(stderr);
+        console.log(stdout);
+        res.redirect(req.session.url || '/');
+      }
+    }
+  );
+});
 
 export default router;
