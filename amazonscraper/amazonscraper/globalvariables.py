6792300902b333a4b203a4f40055e174bb1c008e
Highlighted-dev <bartosz.korz@gmail.com>
2022-06-15 21:58:06 +0200

    Added tutorial how to create urls for amazonProductScraper

--- amazonscraper/amazonscraper/globalvariables.py
+++ amazonscraper/amazonscraper/globalvariables.py
@@ -1,14 +1,20 @@
 #Here are all important variables that you would like to change.
+#HOW TO CREATE WORKING URL
+#1.Go to any amazon category
+#2. Copy its url | Ex. https://www.amazon.de/-/en/gp/browse.html?node=429874031, https://www.amazon.de/-/en/b/node=21617878031
+#3. Remove anything before 'node=xyz'. Ex https://www.amazon.de/-/en/gp/browse.html?node=42987403 ->  https://www.amazon.de/node=42987403, https://www.amazon.de/-/en/b/node=21617878031 -> https://www.amazon.de/node=21617878031
+#4. Add s? before 'node=xyz' and &s=review-count-rank&language=en after it Ex. https://www.amazon.de/node=42987403 -> https://www.amazon.de/s?node=42987403&s=review-count-rank&language=en, https://www.amazon.de/node=21617878031 -> https://www.amazon.de/s?node=21617878031&s=review-count-rank&language=en
+#5. Click enter and copy link from the browser.
 start_urls = [
         'https://www.amazon.de/s?rh=n%3A571860&s=review-count-rank&language=en',
         'https://www.amazon.de/s?rh=n%3A571954&s=review-count-rank&language=en',
         'https://www.amazon.de/s?rh=n%3A1966060031&s=review-count-rank&language=en',
         'https://www.amazon.de/s?rh=n%3A340849031&s=review-count-rank&language=en',
-        'https://www.amazon.de/s?rh=n%3A571860&s=review-count-rank&language=en',
-        'https://www.amazon.de/s?rh=n%3A571954&s=review-count-rank&language=en',
-        'https://www.amazon.de/s?rh=n%3A1966060031&s=review-count-rank&language=en',
-        'https://www.amazon.de/s?rh=n%3A340849031&s=review-count-rank&language=en',
-        'https://www.amazon.de/s?rh=n%3A571860&s=review-count-rank&language=en',
+        'https://www.amazon.de/s?rh=n%3A1981299031&s=review-count-rank&language=en',
+        'https://www.amazon.de/s?rh=n%3A1981666031&s=review-count-rank&language=en',
+        'https://www.amazon.de/s?rh=n%3A429867031&s=review-count-rank&language=en',
+        'https://www.amazon.de/s?rh=n%3A429874031&s=review-count-rank&language=en',
+        'https://www.amazon.de/s?rh=n%3A429870031&s=review-count-rank&language=en',
     ]
 allowed_domains = ['amazon.de','amazon.fr']
 max_pages_per_category_string = "10" #Up to 99
