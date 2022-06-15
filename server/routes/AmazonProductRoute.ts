7efa54d5b38dc55991a2e5308bd661fce6038bb9
Highlighted-dev <bartosz.korz@gmail.com>
2022-06-15 22:02:07 +0200

    Fixed amazonProductData endpoint - before instead of sending json, program was sending object/array

--- server/routes/AmazonProductRoute.ts
+++ server/routes/AmazonProductRoute.ts
@@ -18,11 +18,10 @@ const router: Router = express.Router();
 router.get('/', async (req: Request, res: Response) => {
   res.json(await AmazonProductData.find());
 });
-//Get product by id
-router.get('/id/:id', async (req: Request, res: Response) => {
-  const { id } = req.params;
-  res.json(await AmazonProductData.findById(id));
-});
+//Check for product data
+router.get('/checkProduct/id/:id', async (req: Request, res: Response) =>
+  isProductDataAlreadyInDatabase(req, res)
+);
 //Get product by name
 router.get('/name/:name', async (req: Request, res: Response) => {
   const { name } = req.params;
@@ -32,42 +31,48 @@ router.get('/name/:name', async (req: Request, res: Response) => {
     })
   );
 });
-//Check for product data
-router.get('/checkProduct/id/:id', async (req: Request, res: Response) =>
-  isProductDataAlreadyInDatabase(req, res)
-);
+//Get product by id
+router.get('/id/:id', async (req: Request, res: Response) => {
+  getAmazonProductData(req, res, AmazonProductData);
+});
+
 //Get details by id
 router.get('/details/id/:id', async (req: Request, res: Response) =>
-  getAmazonDetailedData(req, res, AmazonProductDetails)
+  getAmazonProductData(req, res, AmazonProductDetails)
 );
+
 //Get images by id
 router.get('/images/id/:id', (req: Request, res: Response) =>
-  getAmazonDetailedData(req, res, AmazonProductThumbImages)
+  getAmazonProductData(req, res, AmazonProductThumbImages)
 );
-//Get technical details by id
+
+//Get product technical details by id
 router.get('/technicalDetails/id/:id', (req: Request, res: Response) =>
-  getAmazonDetailedData(req, res, AmazonProductTechnicalDetails)
+  getAmazonProductData(req, res, AmazonProductTechnicalDetails)
 );
+
+//Get product about by id
 router.get('/about/id/:id', (req: Request, res: Response) =>
-  getAmazonDetailedData(req, res, AmazonProductAbout)
+  getAmazonProductData(req, res, AmazonProductAbout)
 );
+//Get product high resolution images by id
 router.get('/highResImages/id/:id', (req: Request, res: Response) =>
   getAmazonHighResImages(req, res)
 );
 
-const getAmazonDetailedData = async (
+const getAmazonProductData = async (
   //Request variable, Response variable, name of the model, if program should search for another data
   req: Request,
   res: Response,
   modelName: Model<any, any, any, any>
 ) => {
   const { id } = req.params;
-  const amazon_product_detailed_data = await modelName.find({
+  const amazon_product_data = await modelName.find({
     product_id: id,
   });
   //If json is not empty that means program found data
-  if (amazon_product_detailed_data.length > 3) {
-    res.status(200).json(amazon_product_detailed_data);
+  if (amazon_product_data.length > 0) {
+    res.status(200).json(amazon_product_data);
   } else {
     //Program didn't find any data, but it's okay becouse sometimes there isn't this type of data on Amazon
     res.status(200).json([{}]);
@@ -77,11 +82,11 @@ const getAmazonDetailedData = async (
 //On Amazon, every product has at least 1 image. So If there are no images it means that there isn't any data about the product in database.
 const isProductDataAlreadyInDatabase = async (req: Request, res: Response) => {
   const { id } = req.params;
-  const amazon_product_detailed_data = await AmazonProductThumbImages.find({
+  const amazon_product_data = await AmazonProductThumbImages.find({
     product_id: id,
   });
   //If json is not empty that means program found data
-  if (amazon_product_detailed_data.length > 3) {
+  if (amazon_product_data.length > 3) {
     res.status(200).send();
   }
   //In case if amazon scraper didn't find any items
