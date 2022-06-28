dfcb782672223878d0c8b9c0b39e5a2b0e1aae97
Highlighted-dev <bartosz.korz@gmail.com>
2022-06-28 18:49:58 +0200

    Renamed private route to SignedUserRoute

---
+++ client/src/RouteSettings/SignedUserRoute.tsx
@@ -0,0 +1,9 @@
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../FirebaseAuthentication/AuthContext';
+
+//If user isn't sign in, redirect him to signIn page
+export default function PrivateRoute({ children }: any) {
+  const { currentUser } = useAuth();
+
+  return currentUser ? children : <Navigate to="/signIn" />;
+}
