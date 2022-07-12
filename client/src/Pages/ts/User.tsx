08e127308948e2ab3449cb9c96abce3caf1582fe
Highlighted-dev <bartosz.korz@gmail.com>
2022-07-12 18:51:34 +0200

    Added basic User page functionalities like logout and user informations

--- client/src/Pages/ts/User.tsx
+++ client/src/Pages/ts/User.tsx
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import AuthContext from '../../components/ts/AuthContext';
 import '../css/User.css';
 const User = () => {
-  // const { currentUser, logout } = useAuth();
+  const [authState, setauthState, login, logout] = useContext(AuthContext);
   const navigate = useNavigate();
   const handleLogout = () => {
     try {
-      // logout();
-      navigate('/SignIn');
+      logout();
+      navigate('/Login');
     } catch {
       console.log('Something went wrong when trying to logout');
     }
   };
   return (
     <div id="userPanel">
-      {/* <h1>Email: {currentUser && currentUser.email}</h1> */}
-      <div id="userInformations">{/* <button onClick={handleLogout}> Logout</button> */}</div>
+      <h1>Username: {authState.username}</h1>
+      <h1>Email: {authState.email}</h1>
+      <div id="userInformations">
+        <button onClick={handleLogout}> Logout</button>
+      </div>
     </div>
   );
 };
