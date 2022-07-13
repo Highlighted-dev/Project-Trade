d1de1f66428c7b6cedf00c5bc68649df6c1f5a27
Highlighted-dev <bartosz.korz@gmail.com>
2022-07-13 22:30:02 +0200

    Added register validation to backend

--- server/routes/UserAuthenticationRoute.ts
+++ server/routes/UserAuthenticationRoute.ts
@@ -15,20 +15,63 @@ interface IUserModel {
   password: string;
 }
 
+const doesPasswordHaveCapitalLetter = (password: string) => {
+  //Check if there is any uppercase letter in password. If there is not, return error
+  if (/[A-Z]/.test(password)) return true;
+  return false;
+};
+
+const doesPasswordHaveNumber = (password: string) => {
+  //Check if there is any number in password. If there is not, return error
+  if (/[1-9]/.test(password)) return true;
+  return false;
+};
+
+const isEmailValid = (email: string) => {
+  //Regular Expression validating email with rfc822 standard. If email is not valid, return error. Examples:
+  // asdkladlkaslkaslk  /Not valid
+  // test.com  /Not valid
+  // test@test  /Not valid
+  // test@test.com   /Valid
+  if (
+    /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*(\.\w{2,})+$/.test(
+      email
+    )
+  )
+    return true;
+  return false;
+};
+
 router.post('/register', jsonParser, async (req: Request, res: Response) => {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    await userModel.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: hashedPassword,
-    });
-    res.json({ status: 'ok' });
-  } catch (e) {
-    //TODO User valdidator
-    res.json({
+  // Checks if any value is null. Validates password and email. If password is not valid, return error. If email is not valid, return error.
+  if (
+    req.body.username &&
+    req.body.email &&
+    req.body.password &&
+    doesPasswordHaveCapitalLetter(req.body.password) &&
+    doesPasswordHaveNumber(req.body.password) &&
+    isEmailValid(req.body.email)
+  ) {
+    try {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      await userModel.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword,
+      });
+      res.json({ status: 'ok' });
+    } catch (e) {
+      //TODO User valdidator
+      res.json({
+        status: 'error',
+        message: 'Something went wrong when trying to create User',
+      });
+    }
+  } else {
+    return res.status(400).json({
       status: 'error',
-      error: 'Something went wrong when trying to create User',
+      message:
+        'Request values cannot be null. Password must contain at least one number and one uppercase letter and email must be valid(by rfc822 standard)',
     });
   }
 });
@@ -37,6 +80,7 @@ router.post('/login', jsonParser, async (req: Request, res: Response) => {
     const user = await userModel.findOne({
       email: req.body.email,
     });
+    //bcrypt compares function compares password with hashed password in database. If password is not valid, return error.
     const isPasswordValid = await bcrypt.compare(
       req.body.password,
       user!.password
@@ -55,15 +99,18 @@ router.post('/login', jsonParser, async (req: Request, res: Response) => {
           maxAge: 1000 * 60 * 60 * 168, //7 days,
         });
       }
-      return res.status(200).json({ message: 'login success' });
+      return res
+        .status(200)
+        .json({ status: 'success', message: 'login success' });
     } else {
-      return res.status(400).json({ error: 'User not found' });
+      return res
+        .status(400)
+        .json({ status: 'error', message: 'User not found' });
     }
   } catch (e) {
-    //TODO User valdidator
     res.json({
       status: 'error',
-      error: 'Something went wrong when trying to sign in User',
+      message: 'Something went wrong when trying to sign in User',
     });
   }
 });
@@ -71,7 +118,7 @@ router.post('/login', jsonParser, async (req: Request, res: Response) => {
 // logout user by deleting cookie.
 router.get('/logout', jsonParser, async (req: Request, res: Response) => {
   res.cookie('token', '', { maxAge: 1 });
-  res.status(200).json({ message: 'logout success' });
+  res.status(200).json({ status: 'success', message: 'logout success' });
 });
 
 router.get(
@@ -83,12 +130,16 @@ router.get(
     //If Request token is not set
     if (!request_token) {
       //We want to return status code 200, becouse it just means user did not login yet, it is not an error.
-      return res.status(200).json({ message: 'User is not logged in.' });
+      return res
+        .status(200)
+        .json({ status: 'success', message: 'User is not logged in.' });
     }
     try {
       //Verify token
       if (!jwt.verify(request_token, process.env.JWT_SECRET)) {
-        return res.status(400).json({ error: 'Token is not valid' });
+        return res
+          .status(400)
+          .json({ status: 'error', message: 'Token is not valid' });
       } else {
         auth = true;
       }
@@ -103,7 +154,9 @@ router.get(
       ) as IUserModel;
       userModel.findById(data._id).exec((err, user) => {
         if (err || !user) {
-          return res.status(400).json({ error: 'User not found' });
+          return res
+            .status(400)
+            .json({ status: 'error', message: 'User not found' });
         }
         //If user is found
         const { _id, username, email } = user;
