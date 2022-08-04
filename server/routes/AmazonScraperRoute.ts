1f8e9d0070152cc5d1d85496fbd139818719e50b
Highlighted-dev <bartosz.korz@gmail.com>
2022-08-05 00:37:34 +0200

    Cleaned doubled code

--- server/routes/AmazonScraperRoute.ts
+++ server/routes/AmazonScraperRoute.ts
@@ -3,16 +3,15 @@ import * as stream from 'stream';
 import session from 'express-session';
 const router: Router = express.Router();
 
-router.get('/id/:id', async (req: Request, res: Response) => {
-  const { id } = req.params;
-  /**
-   * TODO: Run this only when theres no items with that id in database
-   * */
+const runAProductScraper = async (
+  req: Request,
+  res: Response,
+  command: String
+) => {
   const { exec } = require('child_process'); //TODO: Change this to import
+  //Execute the command in the child process
   exec(
-    'cd ../amazonscraper & scrapy crawl AmazonOneProductSpider -a prod_id="' +
-      id +
-      '"',
+    command,
     function (error: Error, stderr: stream.Readable, stdout: stream.Readable) {
       if (error) {
         console.log(error);
@@ -24,47 +23,33 @@ router.get('/id/:id', async (req: Request, res: Response) => {
       }
     }
   );
+};
+
+router.get('/id/:id', async (req: Request, res: Response) => {
+  //Get the product id from the url
+  const { id } = req.params;
+  const command =
+    'cd ../amazonscraper & scrapy crawl AmazonOneProductSpider -a prod_id="' +
+    id +
+    '"';
+  runAProductScraper(req, res, command);
 });
 router.get('/highres/id/:id', async (req: Request, res: Response) => {
+  //Get the product id from the url
   const { id } = req.params;
-  /**
-   * TODO: Run this only when theres no items with that id in database
-   * */
-  const { exec } = require('child_process'); //TODO: Change this to import
-  exec(
+  const command =
     'cd ../amazonscraper & scrapy crawl AmazonGetHighResImages -a prod_id="' +
-      id +
-      '"',
-    function (error: Error, stderr: stream.Readable, stdout: stream.Readable) {
-      if (error) {
-        console.log(error);
-        res.sendStatus(500);
-      } else {
-        console.log(stderr);
-        console.log(stdout);
-        res.redirect(req.session.url || '/');
-      }
-    }
-  );
+    id +
+    '"';
+  runAProductScraper(req, res, command);
 });
 router.get('/prices/id/:id', async (req: Request, res: Response) => {
+  //Get the product id from the url
   const { id } = req.params;
-  const { exec } = require('child_process'); //TODO: Change this to import
-  exec(
+  const command =
     'cd ../amazonscraper & scrapy crawl AmazonProductPrices -a prod_id="' +
-      id +
-      '"',
-    function (error: Error, stderr: stream.Readable, stdout: stream.Readable) {
-      if (error) {
-        console.log(error);
-        res.sendStatus(500);
-      } else {
-        console.log(stderr);
-        console.log(stdout);
-        res.redirect(req.session.url || '/');
-      }
-    }
-  );
+    id +
+    '"';
+  runAProductScraper(req, res, command);
 });
-
 export default router;
