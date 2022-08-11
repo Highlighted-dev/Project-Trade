0a83a26ac4ee44fa43f93229f857371ee100b045
Highlighted-dev <bartosz.korz@gmail.com>
2022-08-11 17:23:41 +0200

    Fixed bug: Data wasn't correct with labels, also cleaned code.

--- client/src/components/ts/LineChart.tsx
+++ client/src/components/ts/LineChart.tsx
@@ -6,7 +6,8 @@ import '../css/LineChart.css';
 ChartJS.register(...registerables);
 
 const LineChart = ({ data, labels }: any) => {
-  const [dates, setDates] = useState(labels);
+  const [chartLabels, setChartLabels] = useState(labels);
+  const [chartData, setChartData] = useState(data);
   const chartRef = useRef<ChartJS>(null);
   const options: any = {
     responsive: true,
@@ -55,6 +56,7 @@ const LineChart = ({ data, labels }: any) => {
       },
       legend: {
         position: 'bottom',
+        reverse: true,
         labels: {
           color: 'rgba(37, 34, 59,0.9)',
         },
@@ -75,37 +77,22 @@ const LineChart = ({ data, labels }: any) => {
       gradient.addColorStop(0, 'rgba(37, 34, 59,0.85)');
       gradient.addColorStop(0.5, 'rgba(37, 34, 59,0.5)');
       gradient.addColorStop(1, 'rgba(37, 34, 59,0.1)');
-      const chartData: any = {
-        labels: dates,
-        datasets: [
-          {
-            label: 'Price',
-            data: data,
-            pointBackgroundColor: 'rgba(255, 255, 255,0.9)',
-            borderWidth: 1,
-            borderColor: 'rgb(35, 35, 35)',
-            backgroundColor: gradient,
-            fill: true,
-          },
-        ],
-      };
-      return chartData;
     }
-    const chartData: any = {
-      labels: dates,
+    const chartDataSettings: any = {
+      labels: chartLabels,
       datasets: [
         {
           label: 'Price',
-          data: data,
+          data: chartData,
           pointBackgroundColor: 'rgba(255, 255, 255,0.9)',
           borderWidth: 1,
           borderColor: 'rgb(35, 35, 35)',
-          backgroundColor: 'rgba(37, 34, 59,0.5)',
+          backgroundColor: gradient ? gradient : 'rgba(37, 34, 59,0.9)',
           fill: true,
         },
       ],
     };
-    return chartData;
+    return chartDataSettings;
   };
 
   const getCurrentDate = (date = new Date()) => {
@@ -121,8 +108,10 @@ const LineChart = ({ data, labels }: any) => {
     return date.toISOString().split('T')[0];
   };
   const filterDates = () => {
-    //Delcraing new array for dates
+    //Delcraing new array for labels and data
     const dates_array = [...labels];
+    const data_array = [...data];
+
     //Getting current chart
     const chart = chartRef.current;
 
@@ -132,24 +121,36 @@ const LineChart = ({ data, labels }: any) => {
 
     //If start date is not empty, filter dates_array to only include dates equal start date and end date
     if (start_date && chart) {
+      //Get start date and end date from input
       const start_date_index = dates_array.indexOf(start_date.value);
       const end_date_index = dates_array.indexOf(end_date.value);
 
       //If there indexes are greater than -1 that means start and end dates are in dates_array
       //If start index is greater than end index, there isn't any date in dates_array that would match the start and end dates.
       if (start_date_index > -1 && end_date_index > -1 && start_date_index <= end_date_index) {
+        //If start index is not equal to end index, there is more than one date in dates_array that would match the start and end dates.
         if (start_date_index != end_date_index) {
+          //Filter dates_array to only include dates equal start date and end date
           dates_array.splice(end_date_index + 1, dates_array.length);
           dates_array.splice(0, start_date_index);
-          setDates(dates_array);
+          setChartLabels(dates_array);
+
+          //Filter data_array to only include dates equal start date and end date
+          data_array.splice(end_date_index + 1, dates_array.length);
+          data_array.splice(0, start_date_index);
+          setChartData(data_array);
         } else {
-          setDates(dates_array.slice(start_date_index, end_date_index + 1));
+          //Set chart data and lables to only one data point
+          setChartLabels(dates_array.slice(start_date_index, end_date_index + 1));
+          setChartData(data_array.slice(start_date_index, end_date_index + 1));
         }
         chart.update();
         return;
       }
     }
-    setDates(labels);
+    //If there are no dates beetwen start and end date, set chart data and labels to all dates
+    setChartLabels(labels);
+    setChartData(data);
   };
   useEffect(() => {
     filterDates();
