71dc114f66fbb3d5d51db9f47abbc7f371ad15d9
Highlighted-dev <bartosz.korz@gmail.com>
2022-08-12 13:43:16 +0200

    Changed mongodb url

--- server/index.ts
+++ server/index.ts
@@ -5,25 +5,18 @@ import AmazonScraperRoute from './routes/AmazonScraperRoute';
 import session from 'express-session';
 import UserAuthenticationRoute from './routes/UserAuthenticationRoute';
 import UserFavouritesRoute from './routes/UserFavouritesRoute';
+require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const app: Application = express();
 
 //as=amazonScraper
 
-const url: string = 'mongodb://localhost:27017/ProjectTrade';
+const url: string = process.env.MONGODB_URI!;
 const port: number = 5000;
 
 mongoose
   .connect(url)
-  .then(() =>
-    app.listen(port, () =>
-      console.log(
-        'Database connected: ',
-        url,
-        ' | Its alive on http://localhost:' + port
-      )
-    )
-  )
+  .then(() => app.listen(port, () => console.log('Database connected: ', url)))
   .catch(error => console.log(error.message));
 app.use(cookieParser());
 app.use(
