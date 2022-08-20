c86bf5296796cb5aaea650b0a906c13d93e16aae
Highlighted-dev <bartosz.korz@gmail.com>
2022-08-21 01:13:33 +0200

    Added more reliable unit tests with expected output

--- amazonscraper/tests/test_AmazonProductPrices.py
+++ amazonscraper/tests/test_AmazonProductPrices.py
@@ -1,9 +1,11 @@
+from datetime import date
 import unittest
 import sys
 import os
 #Add parent folder to sys paths
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from tests.responses.__init__ import fake_response_from_file
+#Second import is when you won't use python -m unittest discover
 try:
     from amazonscraper.amazonscraper.spiders.AmazonProductPrices import AmazonProductPrices
 except ImportError:
@@ -11,29 +13,32 @@ except ImportError:
 
 
 class AmazonProductPricesTest(unittest.TestCase):
-
     #Scrapy spider setup
     def setUp(self):
+        #Create spider object
         self.spider = AmazonProductPrices("any")
+        self.expected_output = {\
+            "item1": \
+                {'product_id': 'any', 'product_price': 'â‚¬239.00', 'product_price_date': str(date.today()), 'mongo_db_column_name': 'amazonProductPrices'},\
+            "item2": \
+                {'product_id': 'any', 'product_price': 'â‚¬24.99', 'product_price_date': str(date.today()), 'mongo_db_column_name': 'amazonProductPrices'}\
+            }
 
-    def _test_item_results(self, results, expected_length):
-        count=0
-        #If any product information is None, unit test will fail
+    def _test_item_results(self, results, item_name):
         for item in results:
-            self.assertIsNotNone(item['product_id'])
-            self.assertIsNotNone(item['product_price'])
-            self.assertIsNotNone(item['product_price_date'] )
-            self.assertIsNotNone(item['mongo_db_column_name'] )
-            count+=1
+            #Comapre item with expected output 
+            self.assertEqual(item['product_id'], self.expected_output[item_name]['product_id'])
+            self.assertEqual(item['product_price'], self.expected_output[item_name]['product_price'])
+            self.assertEqual(item['product_price_date'], self.expected_output[item_name]['product_price_date'])
+            self.assertEqual(item['mongo_db_column_name'], self.expected_output[item_name]['mongo_db_column_name'])
             
-        #Check if product has {expected_length} informations
-        self.assertEqual(count, expected_length)
-
     def test_parse(self):
+        #Get results from spider
         results = self.spider.parse(fake_response_from_file('offline_test_pages/unittestpage1.html'))
-        self._test_item_results(results,1)
+        self._test_item_results(results, "item1")
+        
         results = self.spider.parse(fake_response_from_file('offline_test_pages/unittestpage2.html'))
-        self._test_item_results(results,1)
+        self._test_item_results(results, "item2")
 
 if __name__ == '__main__':
     unittest.main()
