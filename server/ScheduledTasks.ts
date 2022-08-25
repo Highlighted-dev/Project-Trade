e66471e975bc1502013f1443e6fc3e46dde22f40
Highlighted-dev <bartosz.korz@gmail.com>
2022-08-25 19:02:06 +0200

    Created ScheduledTasks.ts

---
+++ server/ScheduledTasks.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosResponse } from 'axios';
+import schedule from 'node-schedule';
+
+interface IResponse {
+  status: string;
+  message: string;
+  data: string;
+}
+
+const schedulePriceUpdate = schedule.scheduleJob('00 19 * * *', () => {
+  axios
+    .get('http://localhost:5000/api/ap/updatePrices')
+    .then((response: AxiosResponse) => response.data)
+    .then((responesData: IResponse) => {
+      console.log(responesData.message);
+    })
+    .catch((error: Error) => {
+      console.log(error.message);
+    });
+});
+
+export default schedulePriceUpdate;
