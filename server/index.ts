507f511814ef8b470be793da34b7a5eacb50575b
Highlighted-dev <bartosz.korz@gmail.com>
2022-08-25 19:07:52 +0200

    Adde schedulePriceUpdate

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,11 @@
-import express, { Application, Response, Request } from 'express';
-import mongoose, { Connection } from 'mongoose';
+import express, { Application } from 'express';
+import mongoose from 'mongoose';
 import AmazonProductRoute from './routes/AmazonProductRoute';
 import AmazonScraperRoute from './routes/AmazonScraperRoute';
 import session from 'express-session';
 import UserAuthenticationRoute from './routes/UserAuthenticationRoute';
 import UserFavouritesRoute from './routes/UserFavouritesRoute';
+import schedulePriceUpdate from './ScheduledTasks';
 require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const app: Application = express();
@@ -16,8 +17,17 @@ const port: number = 5000;
 
 mongoose
   .connect(url)
-  .then(() => app.listen(port, () => console.log('Database connected: ', url)))
+  .then(() =>
+    app.listen(port, () =>
+      console.log(
+        'Database connected:',
+        url,
+        '| Server working on http://localhost:' + port
+      )
+    )
+  )
   .catch(error => console.log(error.message));
+
 app.use(cookieParser());
 app.use(
   session({
@@ -31,3 +41,5 @@ app.use('/api/ap', AmazonProductRoute);
 app.use('/api/as', AmazonScraperRoute);
 app.use('/api/auth', UserAuthenticationRoute);
 app.use('/api/favourites', UserFavouritesRoute);
+
+schedulePriceUpdate;
