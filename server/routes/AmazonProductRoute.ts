1d8e384b7839621e0db12e00d923abee6386db51
Highlighted-dev <bartosz.korz@gmail.com>
2022-10-04 22:04:54 +0200

    Fixed sales calculation (now is ugly but is working as intended)

--- server/routes/AmazonProductRoute.ts
+++ server/routes/AmazonProductRoute.ts
@@ -60,9 +60,14 @@ const getAmazonProductData = async (
   }
 };
 
-const getAmazonPrice = async (req: Request, res: Response) => {
+const getAmazonSpecificDataOrUpdateIfNeeded = async (
+  req: Request,
+  res: Response,
+  modelName: Model<any, any, any, any>,
+  scrapingUrl: string
+) => {
   const { id } = req.params;
-  var amazon_prices = await AmazonProductPrices.find({
+  var amazon_data = await modelName.find({
     product_id: id,
   });
 
@@ -70,42 +75,43 @@ const getAmazonPrice = async (req: Request, res: Response) => {
   var yourDate = new Date();
   const offset = yourDate.getTimezoneOffset();
   yourDate = new Date(yourDate.getTime() - offset * 60 * 1000);
+
   if (
     //If program found data and today's date is equal to product_price_date of last update (Ex. 07/07/2022 (today) == 07/07/2022 (last update))
-    amazon_prices.length > 0 &&
+    amazon_data.length > 0 &&
     yourDate.toISOString().split('T')[0] ==
-      amazon_prices[amazon_prices.length - 1].product_price_date
+      amazon_data[amazon_data.length - 1].product_price_date
   )
     res.status(200).json({
       status: 'ok',
-      message: 'Product prices found!',
-      data: amazon_prices,
+      message: 'Product specific data found!',
+      data: amazon_data,
     });
   else {
-    getRequestWithAxios('http://localhost:5000/api/as/prices/id/' + id)
+    getRequestWithAxios(scrapingUrl + id)
       .then(async response => {
         if (response.status == 200) {
-          amazon_prices = await AmazonProductPrices.find({
+          amazon_data = await AmazonProductPrices.find({
             product_id: id,
           });
           //If program succesfuly scraped new data and then found them in database
           if (
-            amazon_prices.length > 0 &&
+            amazon_data.length > 0 &&
             yourDate.toISOString().split('T')[0] ==
-              amazon_prices[amazon_prices.length - 1].product_price_date
+              amazon_data[amazon_data.length - 1].product_price_date
           ) {
             res.status(200).json({
               status: 'ok',
               message:
-                'Product price was successfully scraped and added to database',
-              data: amazon_prices,
+                'Product specific data was successfully scraped and added to database',
+              data: amazon_data,
             });
           } else {
             res.status(404).json({
               status: 'error',
               error: 'NOT FOUND',
               message:
-                'Program tried to scrape product price but it didnt find any new data',
+                'Program tried to scrape product specific data but it didnt find any new data',
             });
           }
         }
@@ -122,7 +128,6 @@ const getRequestWithAxios = async (url: string, params: any = null) => {
     //If params are not null, program will send params to url
     .get(url, params ? { params } : {})
     .then(response => {
-      //If program found data, program will return it further
       return response;
     });
   return axiosResponse;
@@ -201,7 +206,12 @@ router.get('/about/id/:id', (req: Request, res: Response) =>
 );
 //Get product prices by id
 router.get('/prices/id/:id', (req: Request, res: Response) =>
-  getAmazonPrice(req, res)
+  getAmazonSpecificDataOrUpdateIfNeeded(
+    req,
+    res,
+    AmazonProductPrices,
+    'http://localhost:5000/api/as/prices/id/'
+  )
 );
 //Get product reviews by id
 router.get('/reviews/id/:id', (req: Request, res: Response) =>
@@ -211,78 +221,6 @@ router.get('/reviews/id/:id', (req: Request, res: Response) =>
     AmazonProductReviews,
     'http://localhost:5000/api/as/reviews/id/'
   )
-);
-router.get('/sales/id/:id', (req: Request, res: Response) =>
-  /*
-    Becouse we don't have direct data about sales on Amazon, we will estimate it by using their reviews. 
-    By different sources, we can estimate that 10% of customers who bought product will leave a review.
-    Right now we are collecting reviews from 0 to 10 and from 40 to 50.
-    That means we will get first review date(1) and last review date(50)
-    So we will use this formula to estimate sales:
-    (date_of_last_review - date_of_latest_review) = 50 / number_of_days_beetween_dates * 10 = sales_per_day
-    Ex. 07/07/2022 - 07/05/2021 = 50 / 61 * 10 = 8,36
-  */
-  axios
-    .get('http://localhost:5000/api/ap/reviews/id/' + req.params.id)
-    .then(async response => response.data)
-    .then(async responseData => {
-      //sort json object by date
-      const sortJsonObject = (a: any, b: any) => {
-        return (
-          new Date(a.product_rating_date).getTime() -
-          new Date(b.product_rating_date).getTime()
-        );
-      };
-      responseData.data.sort(sortJsonObject);
-
-      const date_of_last_review = new Date(
-        responseData.data[responseData.data.length - 1].product_rating_date
-      ).getTime();
-
-      const date_of_latest_review = new Date(
-        responseData.data[0].product_rating_date
-      ).getTime();
-
-      const difference_between_dates =
-        (date_of_last_review - date_of_latest_review) / (1000 * 3600 * 24);
-
-      const sales_per_day = (50 / difference_between_dates) * 10;
-
-      //Get current date
-      var yourDate = new Date();
-      const formated_data = new Date(
-        yourDate.getTime() - yourDate.getTimezoneOffset() * 60 * 1000
-      )
-        .toISOString()
-        .split('T')[0];
-
-      //Update or create if not exist new document with sales data
-      await amazonProductSales.updateOne(
-        {
-          product_id: req.params.id,
-          product_sales_date: formated_data,
-        },
-        {
-          product_id: req.params.id,
-          product_sales: sales_per_day.toFixed(2),
-          product_sales_date: formated_data,
-        },
-        { new: true, upsert: true }
-      );
-      const amazon_sales = await amazonProductSales.find({
-        product_id: req.params.id,
-      });
-      if (amazon_sales.length > 0)
-        return res.status(200).json({
-          status: 'ok',
-          message: 'Product sales calculated!',
-          data: amazon_sales,
-        });
-      return res.status(404).json({
-        status: 'error',
-        message: 'Product sales not found!',
-      });
-    })
 );
 //Get product high resolution images by id
 router.get('/highResImages/id/:id', (req: Request, res: Response) =>
@@ -317,5 +255,109 @@ router.get('/updatePrices', async (req: Request, res: Response) => {
       axiosErrorHandler(err, res);
     });
 });
+router.get('/sales/id/:id', async (req: Request, res: Response) => {
+  //Get current date
+  var yourDate = new Date();
+  const current_date = new Date(
+    yourDate.getTime() - yourDate.getTimezoneOffset() * 60 * 1000
+  )
+    .toISOString()
+    .split('T')[0];
 
+  let amazon_sales = await amazonProductSales.find({
+    product_id: req.params.id,
+  });
+  if (
+    amazon_sales.length > 0 &&
+    amazon_sales[amazon_sales.length - 1].product_sales_date === current_date
+  ) {
+    return res.status(200).json({
+      status: 'ok',
+      message: 'Sales data was found in database',
+      data: amazon_sales,
+    });
+  }
+  //If there is no data for today, program will firstly scrape new reviews data from amazon, then update sales data
+  getRequestWithAxios(
+    'http://localhost:5000/api/as/reviews/id/' + req.params.id
+  )
+    .then(response => response.data)
+    .then(responseData => {
+      /*
+      Becouse we don't have direct data about sales on Amazon, we will estimate it by using their reviews. 
+      By different sources, we can estimate that 10% of customers who bought product will leave a review.
+      Right now we are collecting reviews from 0 to 10 and from 40 to 50.
+      That means we will get first review date(1) and last review date(50)
+      So we will use this formula to estimate sales:
+      (date_of_last_review - date_of_latest_review) + (current_date - date_of_last_review) = number_of_days_between_reviews
+      Formula: 50 / number_of_days_beetween_dates * 10 = sales_per_day
+      Ex. (07/07/2022 - 07/05/2021) + (09/07/2022 - 07/07/2022) = 50 / 63 * 10 = 7,93
+      */
+      if (responseData.status === 'ok') {
+        axios
+          .get('http://localhost:5000/api/ap/reviews/id/' + req.params.id)
+          .then(response => response.data)
+          .then(async responseData => {
+            //sort json object by date
+            const sortJsonObject = (a: any, b: any) => {
+              return (
+                new Date(a.product_rating_date).getTime() -
+                new Date(b.product_rating_date).getTime()
+              );
+            };
+            responseData.data.sort(sortJsonObject);
+            const date_of_last_review = new Date(
+              responseData.data[
+                responseData.data.length - 1
+              ].product_rating_date
+            );
+
+            const date_of_latest_review = new Date(
+              responseData.data[0].product_rating_date
+            );
+
+            //Remove time, leave only date
+            yourDate.setHours(0, 0, 0, 0);
+            date_of_last_review.setHours(0, 0, 0, 0);
+            date_of_latest_review.setHours(0, 0, 0, 0);
+
+            const difference_between_dates =
+              (date_of_last_review.getTime() -
+                date_of_latest_review.getTime() +
+                yourDate.getTime() -
+                date_of_last_review.getTime()) /
+              (1000 * 3600 * 24);
+            const sales_per_day = (
+              (50 / difference_between_dates) *
+              10
+            ).toFixed(2);
+            await amazonProductSales.updateOne(
+              {
+                product_id: req.params.id,
+                product_sales_date: current_date,
+              },
+              {
+                product_id: req.params.id,
+                product_sales: sales_per_day,
+                product_sales_date: current_date,
+              },
+              { new: true, upsert: true }
+            );
+            amazon_sales = await amazonProductSales.find({
+              product_id: req.params.id,
+            });
+            if (amazon_sales.length > 0)
+              return res.status(200).json({
+                status: 'ok',
+                message: 'Product sales calculated!',
+                data: amazon_sales,
+              });
+            return res.status(404).json({
+              status: 'error',
+              message: 'Product sales not found!',
+            });
+          });
+      }
+    });
+});
 export default router;
