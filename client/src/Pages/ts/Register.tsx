23de7cb4083d0fb4b5dadd21a85997bac55707f6
Highlighted-dev <bartosz.korz@gmail.com>
2022-11-04 00:25:25 +0100

    Moved validation-related stuff from register to Validation.tsx

--- client/src/Pages/ts/Register.tsx
+++ client/src/Pages/ts/Register.tsx
@@ -6,6 +6,13 @@ import 'alertifyjs/build/css/alertify.css';
 import { authContext } from '../../components/ts/AuthContext';
 import { AuthContextType } from '../../@types/AuthContext';
 import '../css/SignPages.css';
+import {
+  doesPasswordMatch,
+  doesPasswordHaveCapitalLetter,
+  doesPasswordHaveNumber,
+  isEmailValid,
+  isBirthDateValid,
+} from '../../components/ts/Validation';
 
 const SignUp = () => {
   const emailRef = useRef() as MutableRefObject<HTMLInputElement>;
@@ -17,73 +24,38 @@ const SignUp = () => {
   const { register } = useContext(authContext) as AuthContextType;
   const [loading, setLoading] = useState<boolean>(false);
 
-  const doesPasswordMatch = () => {
-    // If password and confirm password are equal
-    if (passwordRef.current.value === confirmPasswordRef.current.value) return true;
-    return false;
-  };
-
-  const doesPasswordHaveCapitalLetter = () => {
-    // Check if there is any uppercase letter in password. If there is not, return error
-    if (/[A-Z]/.test(passwordRef.current.value)) return true;
-    return false;
-  };
-
-  const doesPasswordHaveNumber = () => {
-    // Check if there is any number in password. If there is not, return error
-    if (/[1-9]/.test(passwordRef.current.value)) return true;
-    return false;
-  };
-
-  const isEmailValid = () => {
-    // Regular Expression validating email with rfc822 standard. If email is not valid, return error. Examples:
-    // asdkladlkaslkaslk  /Not valid
-    // test.com  /Not valid
-    // test@test  /Not valid
-    // test@test.com   /Valid
-    if (
-      // eslint-disable-next-line no-control-regex
-      /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*(\.\w{2,})+$/.test(
-        emailRef.current.value,
-      )
-    )
-      return true;
-    return false;
-  };
-
-  const isBirthDateValid = (sample_birth_date: Date) => {
-    if (new Date(birthDateRef.current.value) > sample_birth_date) return true;
-    return false;
-  };
-
   const handleSignUp = async () => {
     // If all input values aren't null
     if (
       emailRef.current.value &&
       passwordRef.current.value &&
+      confirmPasswordRef.current.value &&
       usernameRef.current.value &&
       birthDateRef.current.value &&
       sexRef.current.value
     ) {
-      if (!isEmailValid()) return alertify.error('Email is not valid.');
+      const email = emailRef.current.value;
+      const username = usernameRef.current.value;
+      const password = passwordRef.current.value;
+      const confirm_password = confirmPasswordRef.current.value;
+      const birthdate = birthDateRef.current.value;
+      const sex = sexRef.current.value;
+      if (!isEmailValid(email)) return alertify.error('Email is not valid.');
 
-      if (!doesPasswordMatch()) return alertify.error('Passwords do not match.');
+      if (!doesPasswordMatch(password, confirm_password))
+        return alertify.error('Passwords do not match.');
 
-      if (!doesPasswordHaveCapitalLetter())
+      if (!doesPasswordHaveCapitalLetter(password))
         return alertify.error('Password does not have an upper case letter.');
 
-      if (!doesPasswordHaveNumber()) return alertify.error('Password does not have a number.');
+      if (!doesPasswordHaveNumber(password))
+        return alertify.error('Password does not have a number.');
 
-      if (!isBirthDateValid(new Date('01-01-1900')))
+      if (!isBirthDateValid(new Date('01-01-1900'), new Date(birthDateRef.current.value)))
         return alertify.error('Your birtdate cannot be after 01-01-1900.');
 
       try {
         setLoading(true);
-        const email = emailRef.current.value;
-        const username = usernameRef.current.value;
-        const password = passwordRef.current.value;
-        const birthdate = birthDateRef.current.value;
-        const sex = sexRef.current.value;
         register(username, email, password, birthdate, sex);
       } catch {
         alertify.error('Failed to create an account');
@@ -139,5 +111,4 @@ const SignUp = () => {
     </div>
   );
 };
-
 export default SignUp;
