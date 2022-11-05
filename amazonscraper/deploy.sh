ad7a949c8c84bb2e912bc078a581f0761115f67f
Highlighted-dev <bartosz.korz@gmail.com>
2022-11-05 18:50:37 +0100

    Updated deploy.sh files (new server ip)

--- amazonscraper/deploy.sh
+++ amazonscraper/deploy.sh
@@ -1,4 +1,4 @@
 echo "Deploying files to server..."
-scp -i ~/.ssh/project-trade -r * project-trade@130.61.215.100:/home/project-trade/amazonscraper
+scp -i ~/.ssh/ssh-key-2022-08-03.key -r * ubuntu@138.2.128.241:/home/ubuntu/amazonscraper
 echo "============================================================"
 echo "Files deployed successfully"
--- client/deploy.sh
+++ client/deploy.sh
@@ -2,6 +2,6 @@ echo "Building app..."
 npm run build
 
 echo "Deploying files to server..."
-scp -i ~/.ssh/project-trade -r build/* project-trade@130.61.215.100:/var/www/130.61.215.100
+scp -i ~/.ssh/ssh-key-2022-08-03.key -r build/* ubuntu@138.2.128.241:/var/www/138.2.128.241
 echo "============================================================"
 echo "Files deployed successfully"
