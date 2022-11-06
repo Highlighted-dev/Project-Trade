6bc711addc9e6cc847dde88a0f6ead07e9f6af25
Highlighted-dev <bartosz.korz@gmail.com>
2022-11-06 18:16:09 +0100

    Renamed the test

--- client/cypress/e2e/login_test.cy.ts
+++ client/cypress/e2e/login_test.cy.ts
@@ -1,9 +1,10 @@
-describe('test', () => {
-  it('uga buga tabaluga', () => {
+describe('login test', () => {
+  it('Log ins into the website', () => {
     cy.visit('http://localhost:3000');
     cy.findByLabelText('Email').type('Test1@gmail.com');
     cy.findByLabelText('Password').type('Test1@gmail.com');
     cy.findByText('Sign in').click();
+    cy.findByText('Home').should('exist');
   });
 });
 export {};
