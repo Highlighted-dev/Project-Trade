d6e47e8ed0edc24c605275810adc2d4045f40e4f
Highlighted-dev <bartosz.korz@gmail.com>
2022-12-12 22:54:40 +0100

    Renamed ModalContext.d.ts to modal.d.ts

--- client/src/@types/Modal.d.ts
+++ client/src/@types/Modal.d.ts
@@ -3,9 +3,15 @@ import { Dispatch, SetStateAction } from 'react';
 export type ModalContextType = {
   open: boolean | null;
   setOpen: Dispatch<SetStateAction<boolean>>;
+  isSubmitted: boolean | null;
+  setIsSubmitted: Dispatch<SetStateAction<boolean>>;
   modalTitle: string | null;
   setModalTitle: Dispatch<SetStateAction<string>>;
   modalText: string | null;
   setModalText: Dispatch<SetStateAction<string>>;
-  modalSetup: (title: string, text: string) => void;
+  modalSetup: (IModalSetup) => void;
 };
+export interface IModalSetup {
+  title: string;
+  text: string;
+}
