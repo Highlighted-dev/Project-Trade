c71914da47fe50d7040248f1b053447f5c2e4623
Highlighted-dev <bartosz.korz@gmail.com>
2023-03-14 20:50:59 +0100

    Added 2 new tests for AmazonReviewsSpider + optimalization

--- amazonscraper/tests/test_AmazonReviewsSpider.py
+++ amazonscraper/tests/test_AmazonReviewsSpider.py
@@ -19,9 +19,9 @@ class AmazonProductReviewsTest(unittest.TestCase):
     def setUp(self):
         #Create spider object
         self.spider = AmazonReviewsSpider("any", False, True)
-        logging.info(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-        self.expected_output_1 = json.load(open(os.path.abspath(os.path.join(os.path.dirname(__file__))) + "\setup\Expected_results\AmazonReviews_1.json"))
-        self.expected_output_2 = json.load(open(os.path.abspath(os.path.join(os.path.dirname(__file__))) + "\setup\Expected_results\AmazonReviews_2.json"))
+
+    def get_expected_output(self, file_name):
+        return json.load(open(os.path.abspath(os.path.join(os.path.dirname(__file__))) + f"\setup\Expected_results\{file_name}.json"))
 
     def _test_item_results(self, results, expected_length, expected_output):
         count=0
@@ -38,17 +38,12 @@ class AmazonProductReviewsTest(unittest.TestCase):
             
     def test_parse(self):
         #Get results from spider
-        results = self.spider.parse(fake_response_from_file('offline_test_pages/unittest_reviews1.html'))
-        self._test_item_results(results, 10, self.expected_output_1['data'])
-        logging.info("log: Test 1 passed")  
-        
-        results = self.spider.parse(fake_response_from_file('offline_test_pages/unittest_reviews2.html'))
-        self._test_item_results(results, 10, self.expected_output_2['data'])
-        logging.info("log: Test 2 passed") 
+        for which_test_is_it in range(1,5):
 
-        # results = self.spider.parse(fake_response_from_file('offline_test_pages/unittest_reviews3.html'))
-        # self._test_item_results(results, 10)
-        # logging.info("log: Test 3 passed") 
+            results = self.spider.parse(fake_response_from_file(f'offline_test_pages/unittest_reviews{which_test_is_it}.html'))
+            self._test_item_results(results, 10, self.get_expected_output(f"AmazonReviews_{which_test_is_it}")['data'])
+            logging.info(f"log: Test {which_test_is_it} passed")  
+        
 
         # results = self.spider.parse(fake_response_from_file('offline_test_pages/unittest_reviews4.html'))
         # self._test_item_results(results, 10)
