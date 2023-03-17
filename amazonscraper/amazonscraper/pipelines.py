2f5632efc0bfe900a58c1c0387b7bda288ec6e58
Highlighted-dev <bartosz.korz@gmail.com>
2023-03-17 23:25:56 +0100

    Scraped data is now directly inserted in AmazondProductPrices instead of using pipeline

--- amazonscraper/amazonscraper/pipelines.py
+++ amazonscraper/amazonscraper/pipelines.py
@@ -36,20 +36,7 @@ class AmazonscraperPipeline:
                     mongo_db_column_name.replace_one({"product_id":item["product_id"],"product_about":item["product_about"]},item,upsert=True)
                 case 'amazonProductHighResImages':
                     del item['mongo_db_column_name']
-                    mongo_db_column_name.replace_one({"product_id":item["product_id"],"product_highres_image":item["product_highres_image"]},item,upsert=True)
-                case 'amazonProductPrices':
-                    del item['mongo_db_column_name']
-                    if not item["product_price"]:
-                        #Get all items with same product_id from db and sort them by date
-                        all_items_in_database = list(mongo_db_column_name.find({"product_id":item["product_id"]}))
-                        all_items_in_database.sort(key=lambda r: datetime.strptime(r.get('product_price_date'), '%Y-%m-%d'))
-                        #! Sometimes the price is not available, so we need to get the price from the previous day/days and make it as current price
-                        for i in all_items_in_database[::-1]:
-                            if(i["product_price"] != "None"):
-                                item["product_price"] = i["product_price"]
-                                break
-                            
-                    mongo_db_column_name.replace_one({"product_id":item["product_id"],"product_price_date":item["product_price_date"]},item,upsert=True)                 
+                    mongo_db_column_name.replace_one({"product_id":item["product_id"],"product_highres_image":item["product_highres_image"]},item,upsert=True)          
                 case 'None': #TODO add case for AmazonProductSpider
                     del item['mongo_db_column_name']
         except Exception as e:
--- amazonscraper/amazonscraper/spiders/AmazonProductPrices.py
+++ amazonscraper/amazonscraper/spiders/AmazonProductPrices.py
@@ -5,20 +5,22 @@ from amazoncaptcha import AmazonCaptcha
 from ..items import AmazonItemPrice
 import logging
 from scrapy_splash import SplashFormRequest
-from datetime import date
+from datetime import date, datetime
 import pymongo
-
+from json import dumps
 #TODO If you can buy used product instead of new, product prices will scrape the used one | Change to the new one price ex.https://www.amazon.de/dp/B09JQZ5DYM?th=1
 class AmazonProductPrices(scrapy.Spider):
     def __init__(self, prod_id=None, fetch_prod_ids_from_db = False):
         self.start_urls = []
+        self.insert_one_product_to_db = False
+        self.client = pymongo.MongoClient(GlobalVariables.mongoUrl)
+        self.db = self.client[GlobalVariables.mongoDatabase]
         #If we want to scrape only one product, we can this by passing the product id as an argument.
         if(prod_id):
+            self.insert_one_product_to_db = True
             self.start_urls = ["https://www.amazon.de/-/en/dp/"+prod_id]
         elif(fetch_prod_ids_from_db):
-            client = pymongo.MongoClient(GlobalVariables.mongoUrl)
-            db = client[GlobalVariables.mongoDatabase]
-            mongo_db_column_name = db[GlobalVariables.mongoColumn]
+            mongo_db_column_name = self.db[GlobalVariables.mongoColumn]
             prod_ids = mongo_db_column_name.find({}).distinct("product_id")
             if prod_ids:
                 for prod_id in prod_ids:
@@ -28,10 +30,11 @@ class AmazonProductPrices(scrapy.Spider):
                     
     name = 'AmazonProductPrices'
     allowed_domains = GlobalVariables.allowed_domains
+    
     def parse(self, response):
         try:
             prices = AmazonItemPrice()
-            self.product_id = response.url.split('https://www.amazon.de/-/en/dp/')[1]
+            self.product_id = self.getProductId(response)
             if self.checkForCaptcha(response):
                 yield from self.solveCaptcha(response, self.parse)
             else:
@@ -43,12 +46,29 @@ class AmazonProductPrices(scrapy.Spider):
                 prices['product_id'] = self.product_id
                 prices['product_price'] = re.sub(r"[\'\[\]\€]|\bname\b", '', str(current_price)) if current_price else None # remove "/", " ' " and "€" from current price with regex.
                 prices['product_price_date'] = str(current_date)
-                prices['mongo_db_column_name'] = GlobalVariables.mongo_column_prices
+                if not prices["product_price"]:
+                    logging.warning("Unable to get price from url: %s", response.url)
+                    # Get the last price from the database
+                    last_price = list(self.db[GlobalVariables.mongo_column_prices]
+                                      .find({"product_id":prices["product_id"]})
+                                      .sort("product_price_date", pymongo.DESCENDING)
+                                      .limit(1))[0] 
+                    #! Sometimes the price is not available, so we need to get the price from the previous day/days and make it as current price
+                    prices["product_price"] = last_price["product_price"]
+                if(self.insert_one_product_to_db):
+                    self.db[GlobalVariables.mongo_column_prices].insert_one(dict(prices))
                 yield prices
         except Exception as e:
             logging.error("Something went wrong while extracting items\n")
-            logging.error(e)      
+            logging.error(e)    
 
+    def getProductId(self, response):
+        try:
+            return response.url.split("/dp/")[1].split("/")[0]
+        except IndexError:
+            logging.warning("Unable to get product id from url: %s", response.url)
+            return "None"
+        
     def checkForCaptcha(self, response):
         captcha_url = response.xpath('//div[@class="a-row a-text-center"]/img/@src').extract_first()
 
