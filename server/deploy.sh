7cee7cc5530a5b347486321e692cef88858aa8b2
Bartosz <44095998+Highlighted-dev@users.noreply.github.com>
2024-05-01 16:59:29 +0200

    Delete server/deploy.sh

--- server/deploy.sh
+++
@@ -1,4 +0,0 @@
-echo "Deploying files to server..."
-scp -i ~/.ssh/ssh-key-2022-08-03.key -r * ubuntu@138.2.128.241:/home/ubuntu/express
-echo "============================================================"
-echo "Files deployed successfully"
